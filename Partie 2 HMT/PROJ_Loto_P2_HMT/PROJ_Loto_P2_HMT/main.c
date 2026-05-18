344cfd109344650159eff6d73728ed7de7db4d7e
HENRIIIIIIIII <henri.mott@eduvaud.ch>
2026-05-18 16:01:54 +0200

    Partie 2 HMT Start

---
+++ Partie 2 HMT/PROJ_Loto_P2_HMT/PROJ_Loto_P2_HMT/main.c
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: PROJ_Loto_P2_HMT.c
+// Date de cration 		: 18.05.26
+// Date de modification : 18.05.26
+//
+// Auteur 				: Henri Mott
+//
+// Description          : This code has the goal to Show the user what files there able to read.
+//						  and then it will paste all the values that are in the file
+//
+// Remarques			: 
+//
+// Source				: 
+//----------------------------------------------------------------------------------//
+// Directives de compilation
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>	// pour printf
+#include <string.h>	// pour strcat
+
